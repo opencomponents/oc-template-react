def1136adccf0be5fab179b478a4b28918cddf5b
Nick Balestra <nbalestra@opentable.com>
2017-08-27 16:54:33 +0100

    render test

--- packages/oc-template-react/lib/render.js
+++ packages/oc-template-react/lib/render.js
@@ -7,30 +7,33 @@ const createPredicate = require("./to-be-published/get-js-from-url");
 const tryGetCached = require("./to-be-published/try-get-cached");
 
 module.exports = (options, callback) => {
-  const url = options.model.reactComponent.src;
-  const key = options.model.reactComponent.key;
-  const props = options.model.reactComponent.props;
-  const extractor = (key, context) => context.oc.reactComponents[key];
-  const getJsFromUrl = createPredicate({
-    key,
-    url,
-    globals: { React },
-    extractor
-  });
-
-  tryGetCached("reactComponent", key, getJsFromUrl, (err, App) => {
-    try {
-      const reactHtml = ReactDOMServer.renderToString(
-        React.createElement(App, props)
-      );
-      const html = options.template(
-        Object.assign({}, options.model, {
-          __html: reactHtml
-        })
-      );
-      return callback(null, html);
-    } catch (error) {
-      return callback(error);
-    }
-  });
+  try {
+    const url = options.model.reactComponent.src;
+    const key = options.model.reactComponent.key;
+    const props = options.model.reactComponent.props;
+    const extractor = (key, context) => context.oc.reactComponents[key];
+    const getJsFromUrl = createPredicate({
+      key,
+      url,
+      globals: { React },
+      extractor
+    });
+    tryGetCached("reactComponent", key, getJsFromUrl, (err, ReactComponent) => {
+      try {
+        const reactHtml = ReactDOMServer.renderToString(
+          React.createElement(ReactComponent, props)
+        );
+        const html = options.template(
+          Object.assign({}, options.model, {
+            __html: reactHtml
+          })
+        );
+        return callback(null, html);
+      } catch (error) {
+        return callback(error);
+      }
+    });
+  } catch (err) {
+    return callback(err);
+  }
 };
