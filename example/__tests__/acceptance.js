902926742db9a52f192417b7f0771f08c3304e05
Nick Balestra <nbalestra@opentable.com>
2017-10-16 16:23:31 +0100

    wip

--- example/__tests__/acceptance.js
+++ example/__tests__/acceptance.js
@@ -19,7 +19,7 @@ beforeAll(done => {
     local: true,
     discovery: true,
     verbosity: 1,
-    path: path.resolve(path.join(__dirname, "../")),
+    path: path.resolve("example"),
     port: registryPort,
     baseUrl: registryUrl,
     env: { name: "local" },
