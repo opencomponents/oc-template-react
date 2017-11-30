5145900ed97c2af7e16abfb3d096deffb5e1d1e7
Nick Balestra <nbalestra@opentable.com>
2017-11-30 13:18:00 +0000

    utils&tests

--- acceptance-components/react-app/app.js
+++ acceptance-components/react-app/app.js
@@ -1,34 +1,84 @@
 import React from "react";
+// import PropTypes from 'prop-types';
 import styles from "./styles.css";
+import {
+  withDataProvider,
+  withSettingProvider
+} from "oc-template-react-compiler/utils";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {
-      timer: 0
-    };
-  }
+// class App extends React.Component {
 
-  componentDidMount() {
-    this.timerId = setInterval(() => {
-      this.setState({ timer: this.state.timer + 1 });
-    }, 100);
-  }
+//   constructor(props, context) {
+//     super(props, context);
+//     this.props = props;
+//     this.context = context;
+//     this.state = {
+//       timer: 0,
+//       context : this.context
+//     };
+//   }
 
-  componentDidUnmount() {
-    clearTimeout(this.timerId);
-  }
+//   componentDidMount() {
+//     this.timerId = setInterval(() => {
+//       this.setState({ timer: this.state.timer + 1 });
+//     }, 100);
+//   }
 
-  render() {
-    return (
-      <div className={styles.special}>
-        <h1>
-          Hello {this.props.name} {this.state.timer}
-        </h1>
-      </div>
-    );
-  }
-}
+//   componentDidUnmount() {
+//     clearTimeout(this.timerId);
+//   }
 
-export default App;
+//   render() {
+//     return (
+//       <div className={styles.special}>
+//         <h1>
+//           Hello {this.props.name} {this.state.timer}
+//           <pre>
+//            {JSON.stringify(this.state)}
+//           </pre>
+//         </h1>
+//       </div>
+//     );
+//   }
+// }
+
+// App.contextTypes = {
+//   getData: PropTypes.func
+// };
+
+// const withData = Component => {
+//   const Appo = (props, context) => {
+//     const propsWithData = {
+//       ...props,
+//       getData: context.getData
+//     };
+
+//     return (<Component {...propsWithData}/>)
+//   };
+
+//   Appo.contextTypes = {
+//     getData: PropTypes.func
+//   };
+
+//   return Appo;
+// }
+
+const App = props => {
+  // const propsWithColor = {
+  //   ...props,
+  //   getColor: context.getColor
+  // };
+
+  return (
+    <div>
+      Hello {props.name}
+      <pre>{props.getData.toString()}</pre>
+      <p>{props.getSetting("name")}</p>
+      <p>{props.getSetting("version")}</p>
+      <p>{props.getSetting("baseUrl")}</p>
+      <p>{props.getSetting("staticPath")}</p>
+    </div>
+  );
+};
+
+export default withSettingProvider(withDataProvider(App));
--- packages/oc-template-react-compiler/lib/compileView.js
+++ packages/oc-template-react-compiler/lib/compileView.js
@@ -29,30 +29,42 @@ module.exports = (options, callback) => {
     import React from 'react';
     import View from '${viewPath}';
     
-    export default class OCProvider extends React.Component {
+    class OCProvider extends React.Component {
       getChildContext() {
         const getData = (parameters, cb) => {
-          return windows.oc.getData({
+          return window.oc.getData({
             name: this.props._componentName,
             version: this.props._componentVersion,
             baseUrl: this.props._baseUrl,
             parameters
           }, cb);
-        }
-        return { getData };
+        };
+        const getSetting = setting => {
+          const settingHash = {
+            name: this.props._componentName,
+            version: this.props._componentName,
+            baseUrl: this.props._baseUrl,
+            staticPath: this.props._staticPath
+          };
+          return settingHash[setting];
+        };
+        return { getData, getSetting };
       }
     
       render() {
         const { _staticPath, _baseUrl, _componentName, _componentVersion, ...rest } = this.props;        
         return (
-          <View {...rest}/>
+          <View {...rest} />
         );
       }
     }
     
     OCProvider.childContextTypes = {
-      getData: PropTypes.func
+      getData: PropTypes.func,
+      getSetting: PropTypes.func
     };
+
+    export default OCProvider
   `;
 
   const higherOrderViewName = "higherOrderView.js";
