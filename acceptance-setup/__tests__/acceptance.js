5c412b500d1f038490371011a2e89cda8146bf55
Nick Balestra <nbalestra@opentable.com>
2017-12-01 12:56:52 +0000

    removing versions from stored snapshots in acceptance test

--- acceptance-setup/__tests__/acceptance.js
+++ acceptance-setup/__tests__/acceptance.js
@@ -20,6 +20,8 @@ const ocComponentPath = path.join(
 let registry;
 let testServer;
 
+const semverRegex = /\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b/gi;
+
 beforeAll(done => {
   fs.removeSync(path.join(ocComponentPath, "_package"));
   cli.package(
@@ -69,7 +71,8 @@ afterAll(done => {
 test("Registry should correctly serve rendered and unrendered components", done => {
   const rendered = r(registryUrl + `react-app/?name=SuperMario`)
     .then(function(body) {
-      expect(body).toMatchSnapshot();
+      const bodyVersionless = body.replace(semverRegex, "6.6.6");
+      expect(bodyVersionless).toMatchSnapshot();
     })
     .catch(err => expect(err).toBeNull());
 
@@ -80,7 +83,8 @@ test("Registry should correctly serve rendered and unrendered components", done 
     }
   })
     .then(function(body) {
-      expect(body).toMatchSnapshot();
+      const bodyVersionless = body.replace(semverRegex, "6.6.6");
+      expect(bodyVersionless).toMatchSnapshot();
     })
     .catch(err => expect(err).toBeNull());
 
