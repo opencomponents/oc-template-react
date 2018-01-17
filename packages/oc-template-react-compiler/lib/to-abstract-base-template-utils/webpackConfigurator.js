762946c2582681ece2f4899c3d5b6879a430c79d
Nick Balestra <nbalestra@opentable.com>
2018-01-17 13:40:48 +0000

    added autoprefixer support

--- packages/oc-template-react-compiler/lib/to-abstract-base-template-utils/webpackConfigurator.js
+++ packages/oc-template-react-compiler/lib/to-abstract-base-template-utils/webpackConfigurator.js
@@ -28,11 +28,12 @@ module.exports = function webpackConfigGenerator(options) {
         {
           loader: require.resolve("postcss-loader"),
           options: {
-            ident: 'postcss',
+            ident: "postcss",
             plugins: [
               require("postcss-import"),
               require("postcss-extend"),
-              require("postcss-icss-values")
+              require("postcss-icss-values"),
+              require("autoprefixer")
             ]
           }
         }
