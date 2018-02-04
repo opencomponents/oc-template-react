104ce97ddb229b5861614095bdcdcbaf25243320
matteofigus <matteofigus@gmail.com>
2018-02-04 13:32:07 +0000

    Move style tag to the head

--- packages/oc-template-react-compiler/lib/viewTemplate.js
+++ packages/oc-template-react-compiler/lib/viewTemplate.js
@@ -8,12 +8,13 @@ const viewTemplate = ({
   var modelHTML =  model.__html ? model.__html : '';
   var staticPath = model.reactComponent.props._staticPath;
   var props = JSON.stringify(model.reactComponent.props);
+  var cssTag = '<style data-react-root="${reactRoot}">${css}</style>';
   return '<div id="${reactRoot}" class="${reactRoot}">' + modelHTML + '</div>' +
-    '<style>${css}</style>' +
     '<script>' +
     'window.oc = window.oc || {};' +
     'oc.cmd = oc.cmd || [];' +
     'oc.cmd.push(function(oc){' +
+      'oc.$(\\'' + cssTag + '\\').appendTo(document.head);' +
       'oc.requireSeries(${JSON.stringify(externals)}, function(){' +
         'oc.require(' +
           '["oc", "reactComponents", "${bundleHash}"],' + 
