321e3aff0437dc828a7250da7bcf1731ed8fd77b
Nick Balestra <nbalestra@opentable.com>
2018-02-06 11:48:15 +0000

    rb investigations

--- packages/oc-template-react-compiler/lib/compileServer.js
+++ packages/oc-template-react-compiler/lib/compileServer.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
-const async = require("async");
-const compiler = require("oc-webpack").compiler;
-const fs = require("fs-extra");
-const hashBuilder = require("oc-hash-builder");
-const MemoryFS = require("memory-fs");
-const path = require("path");
-const reactComponentWrapper = require("oc-react-component-wrapper");
+const async = require('async');
+const compiler = require('oc-webpack').compiler;
+const fs = require('fs-extra');
+const hashBuilder = require('oc-hash-builder');
+const MemoryFS = require('memory-fs');
+const path = require('path');
+const reactComponentWrapper = require('oc-react-component-wrapper');
 
-const webpackConfigurator = require("oc-webpack").configurator;
-const higherOrderServerTemplate = require("./higherOrderServerTemplate");
+const webpackConfigurator = require('oc-webpack').configurator;
+const higherOrderServerTemplate = require('./higherOrderServerTemplate');
 
 module.exports = (options, callback) => {
   const serverFileName = options.componentPackage.oc.files.data;
   let serverPath = path.join(options.componentPath, serverFileName);
-  if (process.platform === "win32") {
-    serverPath = serverPath.split("\\").join("\\\\");
+  if (process.platform === 'win32') {
+    serverPath = serverPath.split('\\').join('\\\\');
   }
-  const publishFileName = options.publishFileName || "server.js";
+  const publishFileName = options.publishFileName || 'server.js';
   const publishPath = options.publishPath;
-  const stats = options.verbose ? "verbose" : "errors-only";
+  const stats = options.verbose ? 'verbose' : 'errors-only';
   const dependencies = options.componentPackage.dependencies || {};
   const componentName = options.componentPackage.name;
   const componentVersion = options.componentPackage.version;
@@ -31,12 +31,13 @@ module.exports = (options, callback) => {
     componentVersion,
     bundleHashKey: options.compiledViewInfo.bundle.hashKey
   });
-  const higherOrderServerName = "higherOrderServer.js";
+  const higherOrderServerName = 'higherOrderServer.js';
   const higherOrderServerPath = path.join(
     publishPath,
-    "temp",
+    'temp',
     higherOrderServerName
   );
+
   const config = webpackConfigurator({
     serverPath: higherOrderServerPath,
     publishFileName,
@@ -56,7 +57,7 @@ module.exports = (options, callback) => {
         const memoryFs = new MemoryFS(data);
         const compiledServer = memoryFs.readFileSync(
           `/build/${config.output.filename}`,
-          "UTF8"
+          'UTF8'
         );
         fs.writeFile(
           path.join(publishPath, publishFileName),
@@ -67,7 +68,7 @@ module.exports = (options, callback) => {
               err
                 ? null
                 : {
-                    type: "node.js",
+                    type: 'node.js',
                     hashKey: hashBuilder.fromString(compiledServer),
                     src: publishFileName
                   }
