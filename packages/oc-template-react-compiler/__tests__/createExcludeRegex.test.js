bbd7d8471f67696acaa3e4a2c4d2beb09f061db0
Nick Balestra <nick@balestra.ch>
2018-04-30 16:02:05 +0100

    createRegexHelper

---
+++ packages/oc-template-react-compiler/__tests__/createExcludeRegex.test.js
@@ -0,0 +1,17 @@
+const createExcludeRegex = require("../lib/to-abstract-base-template-utils/createExcludeRegex");
+
+test("should create a regex that match against any node module set aside given ones", () => {
+  const regex = createExcludeRegex([
+    "oc-template-react-compiler/utils",
+    "underscore"
+  ]);
+  expect(regex).toBeInstanceOf(RegExp);
+
+  expect(regex.test("node_modules/lodash")).toBe(true);
+  expect(regex.test("node_modules/oc-template-react-compiler")).toBe(true);
+
+  expect(regex.test("node_modules/oc-template-react-compiler/utils")).toBe(
+    false
+  );
+  expect(regex.test("node_modules/underscore")).toBe(false);
+});
